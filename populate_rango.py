e767a1a9152740f1e5a9074f723fd276a40ffeb1
Dave Nestoff <d.nestoff@gmail.com>
2016-11-09 15:00:04 -0600

    Add views/likes to category model and revise user interface

--- populate_rango.py
+++ populate_rango.py
@@ -7,7 +7,7 @@ django.setup()
 from rango.models import Category, Page
 
 def populate():
-  suits_cat = add_cat('Suits')
+  suits_cat = add_cat('Suits', 32, 125)
   
   add_page(cat=suits_cat,
     title = "Blue suit")
@@ -24,6 +24,11 @@ def populate():
 
   add_page(cat=pants_cat, title="Jeans")
 
+  # Print out what we have added for the user
+  for c in Category.objects.all():
+    for p in Page.objects.filter(category=c):
+      print "- {0} - {1}".format(str(c), str(p))
+
 def add_page(cat, title, views=0):
   page = Page.objects.get_or_create(category=cat, title=title)[0]
   page.url = ''
@@ -31,8 +36,10 @@ def add_page(cat, title, views=0):
   page.save()
   return page
 
-def add_cat(name):
+def add_cat(name, views=0, likes=0):
   cat = Category.objects.get_or_create(name=name)[0]
+  cat.views = views
+  cat.likes = likes
   return cat
 
 if __name__ == '__main__':
