013b421f1142ef9a13775fc8e246e3bd43ecd854
Dave Nestoff <d.nestoff@gmail.com>
2016-11-14 22:29:28 -0600

    Map urls to individual category pages

--- rango/models.py
+++ rango/models.py
@@ -6,7 +6,7 @@ class Category(models.Model):
   name = models.CharField(max_length=128, unique=True)
   views = models.IntegerField(default=0)
   likes = models.IntegerField(default=0)
-  slug = models.SlugField(null=True)
+  slug = models.SlugField(default='')
 
   def save(self, *args, **kwargs):
     # prevents slug from changing every time name changes
--- rango/urls.py
+++ rango/urls.py
@@ -5,6 +5,8 @@ from rango import views
 
 urlpatterns = [
   url(r'^$', views.index, name = 'index'),
-  url(r'^about/', views.about, name = 'about'), 
-  url(r'^contact/', views.contact, name = 'contact')
+  url(r'^about/$', views.about, name = 'about'), 
+  url(r'^contact/$', views.contact, name = 'contact'),
+  # <category_name_url> names the parameter
+  url(r'^category/(?P<category_name_url>[\w\-]+)/$', views.category, name = 'category')
 ]
